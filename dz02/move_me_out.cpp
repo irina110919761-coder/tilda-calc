7b79f9bd2f436bff9d49c1d1e263b444f2efd694
irina110919761-coder <irina110919761@gmail.com>
2026-09-10 21:21:18 +0700

    Add homework 02 moved function source

---
+++ dz02/move_me_out.cpp
@@ -0,0 +1,11 @@
+// Подключаю заголовок, где объявлена функция todo_move_me_out.
+#include "move_me_out.h"
+
+// Подключаю библиотеку для вывода текста в консоль.
+#include <iostream>
+
+// 212-Хруцкая: функция вынесена отдельно по условию домашнего задания.
+void todo_move_me_out() {
+	// Печатаю сообщение, которое раньше было в hello_world.cpp.
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
